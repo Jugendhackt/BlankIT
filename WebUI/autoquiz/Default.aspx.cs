7605d727852e93cf853ffae83d853fef7816d0e8
Jan Petersen <Jan Petersen>
2016-06-11 15:09:23 +0200

    updated WebUI (with search bar)

---
+++ WebUI/autoquiz/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace autoquiz
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Suche_TextChanged(object sender, EventArgs e)
+        {
+            if ((bool)sender == true) {
+                
+            }
+        }
+    }
+}
