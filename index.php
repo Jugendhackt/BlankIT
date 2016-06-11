6090919b53500a2846bb68dc2ec490922680af55
dvs23 <dvs23@dvs-board.de>
2016-06-11 21:25:33 +0200

    Added some commentary

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
-<html>
-<head>
-<meta content="charset=UTF-8"/>
-<title>Autoquiz Backend</title>
-</head>
-
-<body>
-
 <?php
+exec("echo 'request:".$_POST['text']." FQ: ".utf8_encode($_POST['fq'])."' >> ./log",$ou,$re);
 #echo utf8_encode("echo \"".$_POST['text']."\" | /srv/http/WordCount | java -jar FindBlank.jar ".$_POST['fq']);
-exec("echo \"".utf8_encode($_POST['text'])."\" | /srv/http/WordCount | java -jar FindBlank.jar ".utf8_encode($_POST['fq']),$output,$return);
+exec("echo \"".utf8_encode($_POST['text'])."\" | /srv/http/WordCount | java -jar /srv/http/FindBlank.jar ".utf8_encode($_POST['fq']),$output,$return);
 foreach($output as $s){
-	echo ($s."<br/>");
+	echo ($s." ");
 }
 echo ("[@24]");
 ?>
-</p>
-</body>
-</html>
