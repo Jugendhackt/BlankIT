fff993cee013bde4f4d79d6e6bb04c9c96900c98
dvs23 <dvs23@dvs-board.de>
2016-06-11 20:20:30 +0200

    Cleaned WC

--- WordCount/main.cpp
+++ WordCount/main.cpp
@@ -1,18 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <boost/regex.hpp>
-#include <boost/algorithm/string/trim_all.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/algorithm/string/regex.hpp>
-#include <iterator>
-#include <Wort.h>
-#include <pthread.h>
-#include <exception>
-#include <future>
-#include <google/sparse_hash_map>
-#include <google/dense_hash_map>
-#include <stdio.h>
 #include <WordCount.h>
 
 #include "mysql_connection.h"
@@ -23,13 +9,6 @@
 #include <cppconn/prepared_statement.h>
 
 using namespace std;
-using namespace boost::algorithm;
-using namespace boost;
-
-
-using google::sparse_hash_map;
-using google::dense_hash_map;
-
 
 sql::Driver* driver;
 sql::Connection* con;
@@ -38,44 +17,29 @@ sql::Statement* stmt;
 
 int main( int argc, char* argv[] )
 {
-    ///Create-Statements in Code!
-
     driver = get_driver_instance();
     con = driver->connect( "tcp://127.0.0.1:3306", "jh", "" );
     con->setSchema( "WC" );
-    sql::Statement *stmt = con->createStatement();
-    stmt->execute("SET collation_connection = 'latin1_general_cs'");
+    sql::Statement* stmt = con->createStatement();
+    stmt->execute( "SET collation_connection = 'latin1_general_cs'" );
     delete stmt;
 
     string full = "";
     string line;
-    //ifstream myfile( "../../../auto.txt");//Wikipedia-txt/20140616-wiki-de_000000.txt" );
+    unsigned int i = 0;
 
-    //if( myfile.is_open() )
-    //{
-        unsigned int i = 0;
+    while( getline( cin, line ) )
+    {
+        if( line == "EOF" )
+            break;
 
-        while( getline( cin, line ) )
-        {
-            if(line=="EOF")
-                break;
-            full += line + "\n";
-            i++;
-        }
-
-        //myfile.close();
-    //}
-
-    //cout << "Hello world!" << endl;
+        full += line + "\n";
+        i++;
+    }
 
     WordCount wc( con );
     wc.readFile( full );
-    //cout << "eingelesen" << endl;
     wc.procData();
-    //cout << "verarbeitet" << endl;
-    /*wc.storeData();
-    cout<<"gespeichert"<<endl;
-    stmt->execute("SELECT D.ID,D.Wort,W.anz FROM dict AS D JOIN worte AS W ON W.ID=D.ID AND W.anz>50 JOIN flags AS F ON F.ID=D.ID AND F.flag = 'satzanf' AND F.anz < W.anz*0.2 WHERE D.Wort REGEXP '^[A-ZÄÖÜ][a-zäöüÄÖÜ]*$';");*/
 
     return 0;
 }
--- WordCount/src/WordCount.cpp
+++ WordCount/src/WordCount.cpp
@@ -5,78 +5,66 @@ WordCount::WordCount( sql::Connection* c )
     con = c;
 }
 
-WordCount::~WordCount()
-{
-    //dtor
-}
-
 int WordCount::readFile( string text )
 {
     strs.clear();
     string fu = text;
 
-    /*vector<string> tete;
-    boost::algorithm::split_regex( tete, ful, boost::regex( "[[][[].*?[]][]]" ) );
-    cout << tete.size() << endl;
+    vector<vector<string>> lstrs;
 
-    for( string fu : tete )
-    {*/
-        vector<vector<string>> lstrs;
-        //cout << fu.length() << endl;
-
-        boost::regex re1( "[\?!;,\()\"\':]|(?<![0-9])[.]" );
-        string full = boost::regex_replace( fu, re1, " $& " );
-        boost::regex re2( "[ ]+" );
-        full = boost::regex_replace( full, re2, " " );
-        boost::regex re3( "([\[][\[])(.*?)([]][]])" );
-        full = boost::regex_replace( full, re3, "$&$2\n" );
+    boost::regex re1( "[\?!;,\()\"\':]|(?<![0-9])[.]" );
+    string full = boost::regex_replace( fu, re1, " $& " );
+    boost::regex re2( "[ ]+" );
+    full = boost::regex_replace( full, re2, " " );
+    boost::regex re3( "([\[][\[])(.*?)([]][]])" );
+    full = boost::regex_replace( full, re3, "$&$2\n" );
 
 
-        vector<string> tem;
-        boost::algorithm::split_regex( tem, full, boost::regex( "(?<![0-9])[.]|[!?\n\r]" ) );
+    vector<string> tem;
+    boost::algorithm::split_regex( tem, full, boost::regex( "(?<![0-9])[.]|[!?\n\r]" ) );
 
-        for( unsigned int j = 0; j < tem.size(); j++ )
+    for( unsigned int j = 0; j < tem.size(); j++ )
+    {
+        vector<string> tem2;
+        boost::algorithm::split( tem2, tem[j], is_any_of( " \n\r" ) );
+        lstrs.push_back( tem2 );
+    }
+
+    ///leere Wörter löschen
+    for( unsigned int k = 0; k < lstrs.size(); k++ )
+    {
+        for( unsigned int j = 0; j < lstrs[k].size(); j++ )
         {
-            vector<string> tem2;
-            boost::algorithm::split( tem2, tem[j], is_any_of( " \n\r" ) );
-            lstrs.push_back( tem2 );
-        }
-
-        ///leere Wörter löschen
-        for( unsigned int k = 0; k < lstrs.size(); k++ )
-        {
-            for( unsigned int j = 0; j < lstrs[k].size(); j++ )
+            if( lstrs[k][j].length() <= 0 )
             {
-                if( lstrs[k][j].length() <= 0 )
-                {
-                    lstrs[k].erase( lstrs[k].begin() + j );
-                    j--;
-                }
-            }
-
-            if( lstrs[k].size() == 0 )
-            {
-                lstrs.erase( lstrs.begin() + k );
-                k--;
+                lstrs[k].erase( lstrs[k].begin() + j );
+                j--;
             }
         }
 
-        //cout << "lstrs.size()" << lstrs.size() << endl;
-        strs.push_back( lstrs );
-    //}
+        if( lstrs[k].size() == 0 )
+        {
+            lstrs.erase( lstrs.begin() + k );
+            k--;
+        }
+    }
 
-    //cout << "strs.size()" << strs.size() << endl;
+    strs.push_back( lstrs );
 
     return 0;
 }
 
-string makeSentence(vector<string> sen){
+string makeSentence( vector<string> sen )
+{
     string ret = "";
-    for(size_t i = 0;i<sen.size(); i++){
-        ret+=sen[i];
-        ret+=" ";
+
+    for( size_t i = 0; i < sen.size(); i++ )
+    {
+        ret += sen[i];
+        ret += " ";
     }
-    ret+=".";
+
+    ret += ".";
     return ret;
 }
 
@@ -98,67 +86,62 @@ int WordCount::procData()
         si = res->getUInt( "count(*)" );
 
     double mid = ( double )su / ( double )si;
-    //cout << mid << endl;
-    sparse_hash_map<string,unsigned int> wos;
+    sparse_hash_map<string, unsigned int> wos;
 
     for( unsigned int l = 0; l < strs.size(); l++ )
     {
         for( unsigned int k = 0; k < strs[l].size(); k++ )
         {
             for( unsigned int j = 0; j < strs[l][k].size(); j++ )
-            {
-                wos[strs[l][k][j]]=0;
-            }
+                wos[strs[l][k][j]] = 0;
         }
     }
-    //cout<<wos.size()<<endl;
 
-    sql::PreparedStatement *loadAnz = con->prepareStatement("select D.ID,D.Wort,W.anz from dict as D join worte as W on D.ID=W.ID AND D.Wort = ?;");
-    for(auto t = wos.begin(); t!=wos.end(); ++t)
+    sql::PreparedStatement* loadAnz = con->prepareStatement( "select D.ID,D.Wort,W.anz from dict as D join worte as W on D.ID=W.ID AND D.Wort = ?;" );
+
+    for( auto t = wos.begin(); t != wos.end(); ++t )
     {
-        loadAnz->setString(1,t->first);
-        sql::ResultSet *res = loadAnz->executeQuery();
+        loadAnz->setString( 1, t->first );
+        sql::ResultSet* res = loadAnz->executeQuery();
 
-        while(res->next())
-        {
-            wos[t->first]=res->getUInt("anz");
-            //if(res->getUInt("anz")<=mid)
-                //cout<<t->first<<"  "<<res->getUInt("anz")<<endl;
-        }
+        while( res->next() )
+            wos[t->first] = res->getUInt( "anz" );
+
         delete res;
     }
+
     delete loadAnz;
-    unsigned int c1=0;
-    unsigned int c2=0;
+    unsigned int c1 = 0;
+    unsigned int c2 = 0;
+
     for( unsigned int l = 0; l < strs.size(); l++ )
     {
         for( unsigned int k = 0; k < strs[l].size(); k++ )
         {
             unsigned int counter = 0;
+
             for( unsigned int j = 0; j < strs[l][k].size(); j++ )
             {
-                if(wos[strs[l][k][j]]<=mid){
+                if( wos[strs[l][k][j]] <= mid )
                     counter++;
-                }
             }
-            if(strs[l][k].size()>3&&(double)counter/(double)strs[l][k].size()>0.25 && (double)counter/(double)strs[l][k].size()<0.75){
-                string s = makeSentence(strs[l][k]);
-                if(!boost::regex_match(s.c_str(),boost::regex(".*?[=*)(:\\]\\[/0-9]+.*?|^[^A-Z].*?"))){
-                    c2++;
-                    cout<<s<<endl;
-                }
-                //cout<<l<<"  "<<k<<" "<<counter<<"/"<<strs[l][k].size()<<endl;
 
-                //cout<<cont.size()<<cont[0]<<endl;
+            if( strs[l][k].size() > 3 && ( double )counter / ( double )strs[l][k].size() > 0.25 && ( double )counter / ( double )strs[l][k].size() < 0.75 )
+            {
+                string s = makeSentence( strs[l][k] );
+
+                if( !boost::regex_match( s.c_str(), boost::regex( ".*?[=*)(:\\]\\[/0-9]+.*?|^[^A-Z].*?" ) ) )
+                {
+                    c2++;
+                    cout << s << endl;
+                }
             }
+
             c1++;
         }
     }
-    cout<<"[@24]"<<endl;
-    //cout<<c2<<"/"<<c1<<endl;
 
-    //"select D.ID,D.Wort,W.anz from dict as D join worte as W on D.ID=W.ID where D.Wort IN();"
-
+    cout << "[@24]" << endl;
 
     return 0;
 }
