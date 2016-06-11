d24dcb085c0f243c5cb3bceb7e0f5afb6c22f5ba
Jan Petersen <Jan Petersen>
2016-06-11 19:05:32 +0200

    WebUI can now send POST´s

--- WebUI/autoquiz/Default.aspx.cs
+++ WebUI/autoquiz/Default.aspx.cs
@@ -1,23 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Net;
+using System.Text;
+using System.IO;
 
 namespace autoquiz
 {
     public partial class Default : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
 
-        }
-
-        protected void Suche_TextChanged(object sender, EventArgs e)
+        protected void SearchButton_Click(object sender, EventArgs e)
         {
-            if ((bool)sender == true) {
-                
+            try
+            {
+                string url = "http://192.168.173.28/index.php";
+                string fq = "2";
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.Method = "POST";
+                string Data = "text=" + SearchBox.Text + "\n fq=" + fq;
+                byte[] postBytes = Encoding.ASCII.GetBytes(Data);
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.ContentLength = Data.Length;
+                Stream requestStream = req.GetRequestStream();
+                requestStream.Write(postBytes, 0, postBytes.Length);
+                requestStream.Close();
+
+                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+                Stream resStream = response.GetResponseStream();
+
+                var sr = new StreamReader(response.GetResponseStream());
+                string responseText = sr.ReadToEnd();
+            }
+            catch (WebException)
+            {
             }
         }
     }
