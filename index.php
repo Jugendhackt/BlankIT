9f624cff698a6d5986749e06aee9a09e8522cfe2
dvs23 <dvs23@dvs-board.de>
2016-06-11 17:07:41 +0200

    Refreshed index.*; ALL WORKING :)

--- index.php
+++ index.php
@@ -8,12 +8,11 @@
 
 <?php
 #echo utf8_encode("echo \"".$_POST['text']."\" | /srv/http/WordCount | java -jar FindBlank.jar ".$_POST['fq']);
-if(strpos($_POST['text'],"[@")!==false)
-	die "Nö."
 exec("echo \"".utf8_encode($_POST['text'])."\" | /srv/http/WordCount | java -jar FindBlank.jar ".utf8_encode($_POST['fq']),$output,$return);
 foreach($output as $s){
 	echo ($s."<br/>");
 }
+echo ("[@24]");
 ?>
 </p>
 </body>
